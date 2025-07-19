c01c315b2c352c99d6e9d14db1d973ed44b5afa6
teohyc2018@gmail.com <you@example.com>
2025-07-19 16:38:31 +0800

    open cv class 5

--- open_cv_class_5.py
+++ open_cv_class_5.py
@@ -101,7 +101,6 @@ print(train_dataset.class_names)
 
 #display sample image from dataset
 class_names = train_dataset.class_names
-
 plt.figure(figsize=(18, 10))
 
 #assume dataset batch_size is at least 32
@@ -207,5 +206,165 @@ for image_batch, labels_batch in test_dataset.take(1):
         plt.axis("off")
 plt.show()
 
-#modelling VGG-16
+#specify model input shape
+input_shape = (DatasetConfig.IMG_HEIGHT, DatasetConfig.IMG_WIDTH, DatasetConfig.CHANNELS)
 
+print('Loading model with ImageNet weights...')
+vgg16_conv_base = tf.keras.applications.vgg16.VGG16(input_shape=input_shape,
+                                                    include_top=False, #we supply our own top 
+                                                    weights='imagenet',
+                                                    )
+vgg16_conv_base.summary()
+
+#freeze initial layers in convolutional base
+#set all layers in convolutional base to Trainable
+vgg16_conv_base.trainable = True
+
+#specify number of layers to fine tune at the end of convolutional base
+num_layers_fine_tune = TrainingConfig.LAYERS_FINE_TUNE
+num_layers = len(vgg16_conv_base.layers)
+
+#freeze the initail layers in convolutional base
+for model_layer in vgg16_conv_base.layers[: num_layers - num_layers_fine_tune]:
+    print(f"FREEZING LAYER: {model_layer}")
+    model_layer.trainable = False
+
+print("\n")
+print(f"Configured to fine tune the last {num_layers_fine_tune} convolutional layers")
+print("\n")
+
+vgg16_conv_base.summary()
+
+
+### VGG-16 ARCHITECTURE ###
+
+#add classifier
+inputs = tf.keras.Input(shape=input_shape)
+
+x = tf.keras.applications.vgg16.preprocess_input(inputs)
+
+x = vgg16_conv_base(x)
+
+#flatten output of convolutional base
+x = layers.Flatten()(x)
+
+#add classifier
+x = layers.Dense(128, activation='relu')(x)
+x = layers.Dropout(TrainingConfig.DROPOUT)(x)
+
+#output layer
+outputs = layers.Dense(DatasetConfig.NUM_CLASSES, activation="softmax")(x)
+
+#final model
+model_vgg16_finetune = keras.Model(inputs, outputs)
+model_vgg16_finetune.summary()
+
+#use intiger encoded labels
+model_vgg16_finetune.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=TrainingConfig.LEARNING_RATE),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=["accuracy"],
+)
+
+#train
+training_results = model_vgg16_finetune.fit(train_dataset,
+                                             epochs=TrainingConfig.EPOCHS,
+                                             validation_data=valid_dataset,
+                                             )
+
+#plot training results
+def plot_results(metrics, ylabel=None, ylim=None, metric_name=None, color=None):
+    fig, ax = plt.subplots(figsize=(15, 4))
+
+    if not (isinstance(metric_name, list) or isinstance(metric_name, tuple)):
+        metrics = [metrics,]
+        metric_name = [metric_name,]
+
+    for idx, metric in enumerate(metrics):
+        ax.plot(metric, color=color[idx])
+    
+    plt.xlabel("Epoch")
+    plt.ylabel(ylabel)
+    plt.title(ylabel)
+    plt.xlim([0, TrainingConfig.EPOCHS -1])
+    plt.ylim(ylim)
+    #tailor x-axis tick marks
+    ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.xaxis.set_major_formatter(FormatStrFormatter("%d"))
+    ax.xaxis.set_major_locator(MultipleLocator(1))
+    plt.grid(True)
+    plt.legend(metric_name)
+    plt.show()
+    plt.close()
+
+#retrieve training results
+train_loss = training_results.history["loss"]
+train_acc = training_results.history["accuracy"]
+valid_loss = training_results.history["val_loss"]
+valid_acc = training_results.history["val_accuracy"]
+
+plot_results(
+    [train_loss, valid_loss],
+    ylabel="Loss",
+    ylim=[0.0, 5.0],
+    metric_name=["Trainig Loss", "Validation Loss"],
+    color=["g", "b"],
+)
+
+plot_results(
+    [train_acc, valid_acc],
+    ylabel="Accuracy",
+    ylim=[0.0, 1.0],
+    metric_name=["Training Accuracy", "Validation Accuracy"],
+    color=["g", "b"],
+)
+
+print(f"Model valid accuracy: {model_vgg16_finetune.evaluate(valid_dataset)[1]*100:.3f}")
+print(f"Model test accuracy: {model_vgg16_finetune.evaluate(test_dataset)[1]*100:.3f}")
+
+#display sample prediction
+def display_predictions(dataset, model, class_names):
+
+    plt.figure(figsize=(20, 20))
+    num_rows = 8
+    num_cols = 8
+    jdx = 0
+
+    #evaluate two batches.
+    for image_batch, labels_batch in dataset.take(2):
+        print(image_batch.shape)
+
+        #prediction for the current batch
+        predictions = model.predict(image_batch)
+
+        #loop over all images
+        for idx in range(len(labels_batch)):
+            pred_idx = tf.argmax(predictions[idx]).numpy()
+            truth_idx = labels_batch[idx].numpy
+
+            #set title color
+            if pred_idx == truth_idx:
+                color = "g"
+            else:
+                color = "r"
+
+            jdx += 1
+
+            if jdx > num_rows * num_cols:
+                #break from loops during max image
+                break
+
+            ax = plt.subplot(num_rows, num_cols, jdx)
+            title = str(class_names[truth_idx]) + " : " + str(class_names[pred_idx])
+
+            title_obj = plt.title(title)
+            plt.setp(title_obj, color=color)
+            plt.axis("off")
+            plt.imshow(image_batch[idx].numpy().astype("uint8"))
+
+    plt.show()
+    return
+
+display_predictions(valid_dataset, model_vgg16_finetune, class_names)
+display_predictions(test_dataset, model_vgg16_finetune, class_names) 
+            
